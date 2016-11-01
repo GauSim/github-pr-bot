d5d2fcbbfdc6832b63021b420c0b37ab554e35f0
Simon Gausmann <Simon.Gausmann@live.de>
2016-11-01 08:01:39 +0100

    playing around

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
-// import * as bodyParser from 'body-parser';
+import { spawn, exec } from 'child_process';
+import { SECRET } from './conf';
 
 const port = process.env.PORT || 8080;
 const createHandler = require('github-webhook-handler')
@@ -7,26 +8,71 @@ const createHandler = require('github-webhook-handler')
 type ParsedRequest = { body: any } & express.Request;
 
 
-const handler = createHandler({ path: '/webhook', secret: 'test' })
-handler.on('*', function (event:{payload:any}) {
+const gitClone = () => {
+  return new Promise((ok, fail) => {
+    const gitClone = spawn('git', ['clone', 'https://github.com/GauSim/private-push.git', 'temp']);
 
+    gitClone.stdout.on('data', (data) => {
+      console.log(`child process stdout: ${data}`);
+    });
+
+    gitClone.stderr.on('data', (data) => {
+      console.log(`child process stderr: ${data}`);
+    });
+
+    gitClone.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      ok(code);
+    });
+  });
+}
+
+const handler = createHandler({ path: '/webhook', secret: SECRET })
+handler.on('*', function (event: { payload: any }) {
+  debugger;
   const { pull_request } = event.payload as {
     action: 'opened';
     pull_request: {
       title: string;
-      user: {}
+      user: {};
+      base: {
+        repo: {
+          full_name: string;
+        };
+      };
+      head: {
+        sha: string;
+      };
     }
   };
 
-  console.log(pull_request);
-  console.log('#############')
-  console.log(pull_request['base']['repo']['full_name'])
+
+  gitClone().then(_ => {
+    exec('cd temp && npm install',
+      (error, stdout, stderr) => {
+        debugger;
+        
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+
+  });
+  console.log(pull_request.base.repo.full_name)
+  console.log(pull_request.head.sha);
+  // git checkout sha-of-commit -b testing-a-commit
+
+
 });
 
+handler.on('error', (error) => {
+  console.log(error);
+})
 
-const webhook = (req: ParsedRequest, res: express.Response, next: () => void) => {
-  handler(req, res, _ => next());
-};
+const webhook = (req: ParsedRequest, res: express.Response, next: () => void) => handler(req, res, _ => next());
 
 var app = express();
 app.use(webhook);
